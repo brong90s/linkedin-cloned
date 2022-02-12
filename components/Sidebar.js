babffbb63e2230b92db346e6f16e76ed139d674b
Oooooiii <hearong.200@gmail.com>
2022-02-12 21:16:53 +0700

    tm: 04:19:53

---
+++ components/Sidebar.js
@@ -0,0 +1,70 @@
+import { Avatar } from "@mui/material"
+import Image from "next/image"
+import BookmarkOutlinedIcon from "@mui/icons-material/BookmarkOutlined"
+import AddRoundedIcon from "@mui/icons-material/AddRounded"
+import React from "react"
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-2 min-w-max max-w-lg">
+      {/* TOP */}
+      <div className="bg-white dark:bg-[#1D2226] rounded-lg overflow-hidden relative flex flex-col items-center text-center border border-gray-300 dark:border-none">
+        <div className="relative w-full h-14">
+          <Image src="https://rb.gy/i26zak" layout="fill" priority />
+        </div>
+        <Avatar
+          src="https://avatars.githubusercontent.com/u/89784728?v=4"
+          className="!h-14 !w-14 !border-2 !absolute !top-4 !cursor-pointer"
+        />
+        <div className="mt-5 py-4 space-x-0.5">
+          <h4 className="hover:underline decoration-purple-700 underline-offset-1 cursor-pointer">
+            Oooooiii
+          </h4>
+          <p className="text-black/60 dark:text-white/75 text-sm">
+            oooooiii@gmail.com
+          </p>
+        </div>
+        <div className="hidden md:inline text-left dark:text-white/75 text-sm">
+          <div className="font-medium sidebarButton space-y-0.5">
+            <div className="flex justify-between space-x-2">
+              <h4>Who viewed your profile</h4>
+              <span className="text-bue-500">343</span>
+            </div>
+            <div className="flex justify-between space-x-2">
+              <h4>Views your post</h4>
+              <span className="text-blue-500">1,848</span>
+            </div>
+          </div>
+          <div className="sidebarButton">
+            <h4 className="leading-4 text-xs">
+              Access exclusive tools & insights
+            </h4>
+            <h4 className="dark:text-white font-medium">
+              <span className="w-3 h-3 bg-gradient-to-tr from-yellow-700 to-yellow-200 inline-block rounded-sm mr-1" />{" "}
+              Try Premium for free
+            </h4>
+          </div>
+
+          <div className="sidebarButton flex items-center space-x-1.5">
+            <BookmarkOutlinedIcon className="!-ml-1" />
+            <h4 className="dark:text-white font-medium">My items</h4>
+          </div>
+        </div>
+      </div>
+      {/* BOTTOM */}
+      <div className="hidden md:flex flex-col space-y-2 pt-2.5 sticky top-20 bg-white dark:bg-[#1D2226] text-black/70 dark:text-white-75 rounded-lg overflow-hidden border border-gray-300 dark:border-none">
+        <p className="sidebarLink">Groups</p>
+        <div className="flex items-center justify-between">
+          <p className="sidebarLink">Events</p>
+          <AddRoundedIcon className="!h-5" />
+        </div>
+        <p className="sidebarLink">Followed Hashtag</p>
+        <div className="sidebarButton text-center">
+          <h4 className="dark:text-white font-medium text-sm">Discover More</h4>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
-import { signOut } from "next-auth/react"
 import Head from "next/head"
 import Header from "../components/Header"
+import Sidebar from "../components/Sidebar"
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
-        <title>LinkedIn</title>
+        <title>Feed | LinkedIn</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Header />
+
+      <main className="flex justify-center gap-x-5 px-4 sm:px-12">
+        <div className="flex flex-col md:flex-row gap-5">
+          {/* SIDEBAR */}
+          <Sidebar />
+          {/* FEED */}
+        </div>
+        {/* WIDGET */}
+      </main>
     </div>
   )
 }
